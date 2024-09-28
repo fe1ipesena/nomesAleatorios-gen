795ea1ba010c2137d1072036f7c965c4ee67190a
Felipe Carvalho Sena <felipecsena877@gmail.com>
2024-09-27 21:16:47 -0300

    starting my app

---
+++ namer_app/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  //receber o próximo nome
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Uma ideia de nome:'),
+          BigCard(pair: pair),
+
+          //adicionando botao
+          ElevatedButton(onPressed: (){
+              appState.getNext(); //já pede o próximo
+          }, 
+          child: Text("Próximo"), 
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asPascalCase);
+  }
+}
